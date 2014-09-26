a6af952adc17508d9423e0e3a918bf3660e2bfd1
Alexander Zakusylo <zakgof@gmail.com>
2014-09-26 12:05:57 +0300

    MemKvs persistense tools added

--- src/main/java/com/zakgof/db/sqlkvs/MemKvs.java
+++ src/main/java/com/zakgof/db/sqlkvs/MemKvs.java
@@ -1,9 +1,13 @@
 package com.zakgof.db.sqlkvs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.zakgof.db.kvs.ITransactionalKvs;
+import com.zakgof.serialize.ZeSerializer;
 
 public class MemKvs implements ITransactionalKvs {
   
@@ -33,6 +37,19 @@ public class MemKvs implements ITransactionalKvs {
   @Override
   public void commit() {
     // TODO Auto-generated method stub
+  }
+
+  public void persist(OutputStream stream) throws IOException {
+    ZeSerializer serializer = new ZeSerializer();
+    serializer.serialize(values, stream);
+    stream.flush();
+    stream.close();
+  }
+
+  public void load(InputStream is) {
+    ZeSerializer serializer = new ZeSerializer();
+    Map<?, ?> map = serializer.deserialize(is, Map.class);
+    this.values.putAll(map);    
   }
 
 }
