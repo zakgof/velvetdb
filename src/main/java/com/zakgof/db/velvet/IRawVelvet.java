ca4bed50d3247fcbd181747ea6147cc7bd5dd923
Alexander Zakusylo <zakgof@gmail.com>
2014-11-24 10:15:20 +0200

    new indexing - wip

--- src/main/java/com/zakgof/db/velvet/IRawVelvet.java
+++ src/main/java/com/zakgof/db/velvet/IRawVelvet.java
@@ -20,7 +20,30 @@ public interface IRawVelvet extends ITransactional, ILockable {
   public void connect(Object key1, Object key2, String edgekind);
   public void disconnect(Object key1, Object key2, String edgekind);
   
+  public <K> IIndex<K> getIndex(Object key, Class<K> clazz);
   
+  public <K> ISortedIndex<K> getSortedIndex(Object key, Class<K> clazz);
+  
+  public interface IIndex<K> {
+    void add(K indexentry);
+    boolean remove(K indexentry);
+    boolean contains(K indexentry);
+    List<K> getAll();
+  }
+  
+  public interface ISortedIndex<K> extends IIndex<K> {
+    interface ICursor {      
+    }
+    ICursor first();
+    ICursor last();
+    ICursor next(ICursor cursor);
+    ICursor prev(ICursor cursor);
+    ICursor greater(K value);
+    ICursor lower(K value);
+    ICursor floor(K value);
+    ICursor ceiling(K value);
+    K get(ICursor cursor);
+  }
   
 
 }
