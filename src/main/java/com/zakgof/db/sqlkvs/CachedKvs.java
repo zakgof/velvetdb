463faf66cbca170500ee8be1b7786ea65f93fbb4
Alexander Zakusylo <zakgof@gmail.com>
2014-12-08 09:46:46 +0200

    bytearrays wrapped to make them cacheable

--- src/main/java/com/zakgof/db/sqlkvs/CachedKvs.java
+++ src/main/java/com/zakgof/db/sqlkvs/CachedKvs.java
@@ -2,6 +2,7 @@ package com.zakgof.db.sqlkvs;
 
 import com.zakgof.db.ICache;
 import com.zakgof.db.kvs.ITransactionalKvs;
+import com.zakgof.tools.Buffer;
 
 public class CachedKvs implements ITransactionalKvs {
 
@@ -15,25 +16,32 @@ public class CachedKvs implements ITransactionalKvs {
 
   @Override
   public <T> T get(Class<T> clazz, Object key) {
-    T cached = clazz.cast(cache.get(key));
+    T cached = clazz.cast(cache.get(convert(key)));
     if (cached != null)
       return cached;
     
     T value = kvs.get(clazz, key);
-    cache.put(key, value);
+    cache.put(convert(key), value);
     
     return value;
   }
 
+  private static Object convert(Object key) {
+    // TODO Auto-generated method stub
+    if (key instanceof byte[])
+      return new Buffer((byte[])key);
+    return key;
+  }
+
   @Override
   public <T> void put(Object key, T value) {
-    cache.put(key, value);
+    cache.put(convert(key), value);
     kvs.put(key, value);
   }
 
   @Override
   public void delete(Object key) {
-    cache.remove(key);
+    cache.remove(convert(key));
     kvs.delete(key);
   }
 
