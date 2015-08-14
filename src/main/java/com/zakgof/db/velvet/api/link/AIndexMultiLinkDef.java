2a99ec66a91eebbdf156b4e2b5bd1fd59539a533
Alexander Zakusylo <zakgof@gmail.com>
2015-08-14 12:24:37 +0300

    Missing files added

---
+++ src/main/java/com/zakgof/db/velvet/api/link/AIndexMultiLinkDef.java
@@ -0,0 +1,35 @@
+package com.zakgof.db.velvet.api.link;
+
+import java.util.List;
+
+import com.zakgof.db.velvet.IVelvet;
+import com.zakgof.db.velvet.IVelvet.IKeyIndexLink;
+import com.zakgof.db.velvet.api.entity.IEntityDef;
+import com.zakgof.db.velvet.api.query.IIndexQuery;
+
+abstract class AIndexMultiLinkDef<HK, HV, CK, CV, M extends Comparable<M>> extends MultiLinkDef<HK, HV, CK, CV> implements IIndexedMultiLink<HK, HV, CK, CV, M> {
+
+  public AIndexMultiLinkDef(IEntityDef<HK, HV> hostEntity, IEntityDef<CK, CV> childEntity) {
+    super(hostEntity, childEntity);
+  }
+
+  abstract protected IKeyIndexLink<CK> index(IVelvet velvet, HK akey);
+
+  @Override
+  public IMultiGetter<HK, HV, CK, CV> indexed(IIndexQuery indexQuery) {
+
+    return new IMultiGetter<HK, HV, CK, CV>() {
+
+      @Override
+      public List<CV> multi(IVelvet velvet, HV node) {
+        return getChildEntity().getAll(velvet, multiKeys(velvet, getHostEntity().keyOf(node)));
+      }
+
+      @Override
+      public List<CK> multiKeys(IVelvet velvet, HK akey) {
+        return index(velvet, akey).linkKeys(getChildEntity().getKeyClass(), indexQuery);
+      }
+    };
+  }
+
+}
