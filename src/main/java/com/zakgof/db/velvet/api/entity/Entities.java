b209a0acda33ffde0cdd0868958c6adaac6f5781
Alexander Zakusylo <zakgof@gmail.com>
2015-09-20 10:40:35 +0300

    Fixed keyless def creation

--- src/main/java/com/zakgof/db/velvet/api/entity/Entities.java
+++ src/main/java/com/zakgof/db/velvet/api/entity/Entities.java
@@ -13,17 +13,19 @@ import com.zakgof.db.velvet.api.entity.impl.SortedEntityDef;
 public enum Entities {
   ;
 
-  @SuppressWarnings("unchecked")
   public static <K, V> IEntityDef<K, V> anno(Class<V> valueClass) {
-    if (valueClass.getAnnotation(Keyless.class) != null) {      
-      return (IEntityDef<K, V>) new KeylessEntityDef<V>(valueClass, AnnoEntityDef.kindOf(valueClass));
-    }
+    if (valueClass.getAnnotation(Keyless.class) != null)     
+      throw new RuntimeException("Use sortedAnno for a keyless entity");
     AnnoKeyProvider<K, V> annoKeyProvider = new AnnoKeyProvider<K, V>(valueClass);
     return new AnnoEntityDef<>(valueClass, annoKeyProvider);
   }
-  
  
+  @SuppressWarnings("unchecked")
   public static <K extends Comparable<K>, V> ISortableEntityDef<K, V> sortedAnno(Class<V> valueClass) {
+    // TODO: should not have both @Key and @Keyless 
+    if (valueClass.getAnnotation(Keyless.class) != null) {      
+      return (ISortableEntityDef<K, V>) new KeylessEntityDef<V>(valueClass, AnnoEntityDef.kindOf(valueClass));
+    }
     AnnoKeyProvider<K, V> annoKeyProvider = new AnnoKeyProvider<K, V>(valueClass);
     return new SortedAnnoEntityDef<>(valueClass, annoKeyProvider);
   }
@@ -36,7 +38,7 @@ public enum Entities {
     return new SortedEntityDef<>(keyClass, valueClass, kind, keyProvider);
   }
 
-  public static <V> IEntityDef<Long, V> keyless(Class<V> clazz, String kind) {
+  public static <V> ISortableEntityDef<Long, V> keyless(Class<V> clazz, String kind) {
     return new KeylessEntityDef<V>(clazz, kind);
   }
 
