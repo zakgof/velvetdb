1416b20473b1e5f6c1976437bef690f5e45b5128
Alexander Zakusylo <zakgof@gmail.com>
2016-07-15 14:46:16 +0300

    Configurable serializers

--- src/main/java/com/zakgof/db/velvet/IVelvetEnvironment.java
+++ src/main/java/com/zakgof/db/velvet/IVelvetEnvironment.java
@@ -1,7 +1,12 @@
 package com.zakgof.db.velvet;
 
+import java.util.function.Supplier;
+
 import com.zakgof.db.txn.ITransactionalEnvironment;
+import com.zakgof.serialize.ISerializer;
 
 public interface IVelvetEnvironment extends ITransactionalEnvironment<IVelvet>, AutoCloseable {
   void close();
+  
+  void setSerializer(Supplier<ISerializer> serializer);
 }
