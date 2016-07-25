651d49ce837f76e6c7f0a1f471123826a358952c
Alexander Zakusylo <zakgof@gmail.com>
2016-07-25 14:02:56 +0300

    Remove absolute paths from tests

--- velvetdb-core/src/test/java/com/zakgof/db/velvet/test/VelvetTestSuite.java
+++ velvetdb-core/src/test/java/com/zakgof/db/velvet/test/VelvetTestSuite.java
@@ -1,6 +1,8 @@
 package com.zakgof.db.velvet.test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.function.Supplier;
 
 import org.junit.AfterClass;
@@ -9,6 +11,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.zakgof.db.velvet.IVelvetEnvironment;
+import com.zakgof.db.velvet.VelvetException;
 import com.zakgof.db.velvet.VelvetFactory;
 
 @RunWith(Suite.class)
@@ -30,16 +33,21 @@ public abstract class VelvetTestSuite {
 
     protected static Supplier<IVelvetEnvironment> velvetProvider;
     private static IVelvetEnvironment env;
-    private static String PATH = "D:/Pr/lab/xodustest/"; // TODO
+    private static String PATH;
 
     protected static void setup(String providerName) {
+        try {
+            PATH = Files.createTempDirectory("velvet").toString();
+        } catch (IOException e) {
+            throw new VelvetException(e);
+        }
         velvetProvider = () -> createVelvet(providerName);
     }
 
     private static IVelvetEnvironment createVelvet(String providerName) {
         tearDownClass();
         new File(PATH).mkdirs();
-        env = VelvetFactory.open("velvetdb://" + providerName + "/" + PATH);
+        env = VelvetFactory.open("velvetdb://" + providerName + "/" + PATH.replace(File.separatorChar, '/'));
         // env.setSerializer(() -> new KryoSerializer());
         // env.setSerializer(() -> new ElsaVelvetSerializer());
         return env;
