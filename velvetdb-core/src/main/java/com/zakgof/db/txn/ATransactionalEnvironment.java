011a101a3f265c49401212844d8d5ff23a41008d
Alexander Zakusylo <zakgof@gmail.com>
2016-12-21 13:13:16 +0200

    Tools update. Removed double exception intercept

--- velvetdb-core/src/main/java/com/zakgof/db/txn/ATransactionalEnvironment.java
+++ velvetdb-core/src/main/java/com/zakgof/db/txn/ATransactionalEnvironment.java
@@ -1,17 +1,11 @@
 package com.zakgof.db.txn;
 
-import com.zakgof.db.velvet.VelvetException;
-
 public abstract class ATransactionalEnvironment<H> implements ITransactionalEnvironment<H> {
 
 	@SuppressWarnings("unchecked")
 	public <R> R calculate(ITransactionCalc<H, R> transaction) {
 		Object[] result = new Object[1];
-		try {
-			execute(h -> result[0] = transaction.execute(h));
-		} catch (Throwable e) {
-			new VelvetException(e);
-		}
+		execute(h -> result[0] = transaction.execute(h));
 		return (R) result[0];
 	}
 }
