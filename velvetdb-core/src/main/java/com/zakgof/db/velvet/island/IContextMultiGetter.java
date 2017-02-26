a25d4a7c974669115452f2787449cf879cd8ba0a
zakgof <zakgof@gmail.com>
2017-02-26 21:36:52 +0200

    Fixed merge fault

--- velvetdb-core/src/main/java/com/zakgof/db/velvet/island/IContextMultiGetter.java
+++ velvetdb-core/src/main/java/com/zakgof/db/velvet/island/IContextMultiGetter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zakgof.db.velvet.island;
 
 import java.util.stream.Stream;
@@ -7,19 +6,5 @@ import com.zakgof.db.velvet.IVelvet;
 import com.zakgof.db.velvet.island.IslandModel.IIslandContext;
 
 interface IContextMultiGetter<T> {
-    public Stream<T> multi(IVelvet velvet, IIslandContext context);
+	public Stream<T> multi(IVelvet velvet, IIslandContext context);
 }
-=======
-package com.zakgof.db.velvet.island;
-
-import java.util.stream.Stream;
-
-import com.zakgof.db.velvet.IVelvet;
-import com.zakgof.db.velvet.island.IslandModel.IIslandContext;
-
-interface IContextMultiGetter<T> {
-    public Stream<T> multi(IVelvet velvet, IIslandContext context);
-
-    public String kind();
-}
->>>>>>> branch 'master' of https://github.com/zakgof/velvetdb
