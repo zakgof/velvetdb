ccd68687a6694df4c4eb5647b06c2ce4716b2e03
Alexander Zakusylo <zakgof@gmail.com>
2017-02-26 21:36:52 +0200

    Fixed merge fault

--- velvetdb-core/src/main/java/com/zakgof/db/velvet/island/IContextSingleGetter.java
+++ velvetdb-core/src/main/java/com/zakgof/db/velvet/island/IContextSingleGetter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zakgof.db.velvet.island;
 
 import com.zakgof.db.velvet.IVelvet;
@@ -7,15 +6,3 @@ import com.zakgof.db.velvet.island.IslandModel.IIslandContext;
 public interface IContextSingleGetter<T> {
     public T single(IVelvet velvet, IIslandContext context);
 }
-=======
-package com.zakgof.db.velvet.island;
-
-import com.zakgof.db.velvet.IVelvet;
-import com.zakgof.db.velvet.island.IslandModel.IIslandContext;
-
-public interface IContextSingleGetter<T> {
-    public T single(IVelvet velvet, IIslandContext context);
-
-    public String kind();
-}
->>>>>>> branch 'master' of https://github.com/zakgof/velvetdb
