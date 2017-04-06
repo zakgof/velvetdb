a1b4de2fa82561e8a83a0ce01d40fa15bf58ba3a
Alexander Zakusylo <zakgof@gmail.com>
2017-04-06 11:55:26 +0300

    Code formatting on link interfaces

--- velvetdb-core/src/main/java/com/zakgof/db/velvet/query/IQueryAnchor.java
+++ velvetdb-core/src/main/java/com/zakgof/db/velvet/query/IQueryAnchor.java
@@ -1,11 +1,11 @@
 package com.zakgof.db.velvet.query;
 
 public interface IQueryAnchor<K, M extends Comparable<? super M>> {
-  
-  boolean isIncluding();
 
-  K getKey();
-  
-  M getMetric();
-  
+    boolean isIncluding();
+
+    K getKey();
+
+    M getMetric();
+
 }
