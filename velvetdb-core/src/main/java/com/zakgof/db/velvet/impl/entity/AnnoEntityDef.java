04b0c5fca41fbb276ba4ffc2d207a61fbe66da8e
Oleksandr Zakusilo <Oleksandr.Zakusilo@halliburton.com>
2017-09-26 17:59:15 +0300

    Cleaner entity creation API

--- velvetdb-core/src/main/java/com/zakgof/db/velvet/impl/entity/AnnoEntityDef.java
+++ velvetdb-core/src/main/java/com/zakgof/db/velvet/impl/entity/AnnoEntityDef.java
@@ -11,34 +11,34 @@ import com.zakgof.db.velvet.properties.IPropertyAccessor;
 
 public class AnnoEntityDef<K, V> extends EntityDef<K, V> implements IPropertyAccessor<K, V> {
 
-  private AnnoKeyProvider<K, V> annoKeyProvider;
+    private AnnoKeyProvider<K, V> annoKeyProvider;
 
-  public AnnoEntityDef(Class<V> valueClass, AnnoKeyProvider<K, V> annoKeyProvider, List<IStoreIndexDef<?, V>> indexes) {
-    super(annoKeyProvider.getKeyClass(), valueClass, kindOf(valueClass), annoKeyProvider, indexes);
-    this.annoKeyProvider = annoKeyProvider;
-  }
+    public AnnoEntityDef(Class<V> valueClass, String kind, AnnoKeyProvider<K, V> annoKeyProvider, List<IStoreIndexDef<?, V>> indexes) {
+        super(annoKeyProvider.getKeyClass(), valueClass, kind, annoKeyProvider, indexes);
+        this.annoKeyProvider = annoKeyProvider;
+    }
 
-  public static String kindOf(Class<?> clazz) {
-    Kind annotation = clazz.getAnnotation(Kind.class);
-    if (annotation != null)
-      return annotation.value();
-    String kind = clazz.getSimpleName().toLowerCase(Locale.ENGLISH);
-    return kind;
-  }
+    public static String kindOf(Class<?> clazz) {
+        Kind annotation = clazz.getAnnotation(Kind.class);
+        if (annotation != null)
+            return annotation.value();
+        String kind = clazz.getSimpleName().toLowerCase(Locale.ENGLISH);
+        return kind;
+    }
 
-  @Override
-  public Collection<String> getProperties() {
-    return annoKeyProvider.getProperties();
-  }
+    @Override
+    public Collection<String> getProperties() {
+        return annoKeyProvider.getProperties();
+    }
 
-  @Override
-  public IProperty<?, V> get(String property) {
-    return annoKeyProvider.get(property);
-  }
+    @Override
+    public IProperty<?, V> get(String property) {
+        return annoKeyProvider.get(property);
+    }
 
-  @Override
-  public IProperty<K, V> getKey() {
-    return annoKeyProvider.getKey();
-  }
+    @Override
+    public IProperty<K, V> getKey() {
+        return annoKeyProvider.getKey();
+    }
 
 }
