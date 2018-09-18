45ca815cb3aed0eb367e15e30a72a23f25ade7f3
Alexander Zakusylo <zakgof@gmail.com>
2018-09-18 17:52:48 +0300

    Code formatting

--- velvetdb-serializer-elsa/src/main/java/com/zakgof/velvetdb/serialize/ElsaVelvetSerializer.java
+++ velvetdb-serializer-elsa/src/main/java/com/zakgof/velvetdb/serialize/ElsaVelvetSerializer.java
@@ -1,4 +1,5 @@
 package com.zakgof.velvetdb.serialize;
+
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -13,40 +14,42 @@ import com.zakgof.serialize.ISerializer;
 
 public class ElsaVelvetSerializer implements ISerializer {
 
-  private ElsaSerializer elsa;
+    private ElsaSerializer elsa;
 
-  public ElsaVelvetSerializer() {
-    this(new ElsaMaker().make());
-  }
-
-  public ElsaVelvetSerializer(ElsaSerializer elsa) {
-    this.elsa = elsa;
-  }
-
-  public <T> byte[] serialize(T object, Class<T> clazz) {
-    ByteArrayOutputStream bas = new ByteArrayOutputStream();
-    DataOutputStream dos = new DataOutputStream(bas);
-    try {
-      elsa.serialize(dos, object);
-    } catch (IOException e) {
-      throw new VelvetException(e);
+    public ElsaVelvetSerializer() {
+        this(new ElsaMaker().make());
     }
-    return bas.toByteArray();
-  }
 
-  public <T> T deserialize(InputStream stream, Class<T> clazz) {
-    DataInputStream in = new DataInputStream(stream);
-    T object;
-    try {
-      object = elsa.deserialize(in);
-    } catch (IOException e) {
-      throw new VelvetException(e);
+    public ElsaVelvetSerializer(ElsaSerializer elsa) {
+        this.elsa = elsa;
     }
-    return object;
-  }
 
-  public ElsaSerializer getElsa() {
-    return elsa;
-  }
+    @Override
+    public <T> byte[] serialize(T object, Class<T> clazz) {
+        ByteArrayOutputStream bas = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bas);
+        try {
+            elsa.serialize(dos, object);
+        } catch (IOException e) {
+            throw new VelvetException(e);
+        }
+        return bas.toByteArray();
+    }
+
+    @Override
+    public <T> T deserialize(InputStream stream, Class<T> clazz) {
+        DataInputStream in = new DataInputStream(stream);
+        T object;
+        try {
+            object = elsa.deserialize(in);
+        } catch (IOException e) {
+            throw new VelvetException(e);
+        }
+        return object;
+    }
+
+    public ElsaSerializer getElsa() {
+        return elsa;
+    }
 
 }
