4318387e4ace79e3803607f3cfbb86ed77afaa9a
Alexander Zakusylo <zakgof@gmail.com>
2020-06-28 19:46:50 +0300

    Source code formatting

--- velvetdb-core/src/main/java/com/zakgof/db/velvet/properties/IProperty.java
+++ velvetdb-core/src/main/java/com/zakgof/db/velvet/properties/IProperty.java
@@ -1,19 +1,19 @@
 package com.zakgof.db.velvet.properties;
 
 public interface IProperty<P, V> {
-  
-  public default boolean isSettable() {
-    return false;
-  }
-  
-  public P get(V instance);
-  
-  public default V put(V instance, P propValue) {
-    throw new UnsupportedOperationException();
-  }
-  
-  public Class<P> getType();
 
-  public String getName();
+    public default boolean isSettable() {
+        return false;
+    }
+
+    public P get(V instance);
+
+    public default V put(V instance, P propValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Class<P> getType();
+
+    public String getName();
 
 }
